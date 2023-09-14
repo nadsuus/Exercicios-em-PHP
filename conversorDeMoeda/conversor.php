c32a06324a1d39f43eaf12f1d970e50be087df04
nadsuus <nadsontvw@gmail.com>
2023-09-14 16:36:00 -0300

    Teste 2

---
+++ conversorDeMoeda/conversor.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<header>
+
+<h1>Conversor de Moeda v1.0</h1>
+
+</header>
+
+<main>
+
+<?php 
+
+$carteira = $_GET["idQuantidadeEmReal"] ?? "O numero não foi digitado ou foi enviado nulo";
+$presoDoDolar = "4,87";
+
+echo $carteira;
+
+?>
+
+</main>
+
+</body>
+</html>
---
+++ conversorDeMoeda/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+<header>
+
+<h1>Conversor de Moeda v1.0</h1>
+
+</header>
+
+<main>
+
+<section>
+
+<form action="conversor.php" method="get">
+
+<label for="QuantidadeEmReal"> Quanto R$ você tem na carteira</label>
+<input type="number" name="QuantidadeEmReal" id="idQuantidadeEmReal">
+
+<input type="submit" value="Enviar">
+</form>
+
+</section>
+
+</main>
+
+</body>
+</html>
